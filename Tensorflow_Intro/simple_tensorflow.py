b241ec1753d9cd41c613f6759c5e5ab6fa705271
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-06-15 17:11:38 +0530

    Intro to Tensorflow

---
+++ Tensorflow_Intro/simple_tensorflow.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+x = tf.constant(1.0, name='input')
+w = tf.Variable(0.8, name='weight')
+y = tf.multiply(w, x, name='output')
+y_ = tf.constant(0.0, name='correct_value')
+loss = tf.pow(y - y_, 2, name='loss')
+train_step = tf.train.GradientDescentOptimizer(0.025).minimize(loss)
+
+for value in [x, w, y, y_, loss]:
+    tf.summary.scalar(value.op.name, value)
+
+summaries = tf.summary.merge_all()
+
+sess = tf.Session()
+summary_writer = tf.summary.FileWriter('log_simple_stats', sess.graph)
+
+sess.run(tf.initialize_all_variables())
+for i in range(100):
+    summary_writer.add_summary(sess.run(summaries), i)
+    sess.run(train_step)
