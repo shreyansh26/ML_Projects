12bcdc96bb2b8520ba92f30dad63af8b0d4d1992
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-06-17 17:29:31 +0530

    Code modification

--- Classifiers/IrisDataset/basicClassifier4_Iris.py
+++ Classifiers/IrisDataset/basicClassifier4_Iris.py
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
 # Decision Tree Classifier
 my_classifier = DecisionTreeClassifier()
-my_classifier = my_classifier.fit(X_train, y_train)
+my_classifier.fit(X_train, y_train)
 
 print("Decision Tree Classifier")
 predictions = my_classifier.predict(X_test)
@@ -30,7 +30,7 @@ print()
 
 # KNeighbor Classifier
 my_classifier2 = KNeighborsClassifier()
-my_classifier2 = my_classifier2.fit(X_train, y_train)
+my_classifier2.fit(X_train, y_train)
 
 print("KNeighbor Classifier")
 predictions2 = my_classifier2.predict(X_test)
