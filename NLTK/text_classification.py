cfc3bc3cd37965c3dec12c88662161755e80c99f
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2017-07-07 17:45:47 +0530

    Add scikit-learn models to test accuracy

--- NLTK/text_classification.py
+++ NLTK/text_classification.py
@@ -3,6 +3,11 @@ import random
 import string
 from nltk.corpus import movie_reviews, stopwords
 import pickle
+from nltk.classify.scikitlearn import SklearnClassifier
+
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
 
 stop_words = set(stopwords.words("english"))
 
@@ -47,11 +52,49 @@ classifer = nltk.NaiveBayesClassifier.train(list(training_set))
 #classifer = pickle.load(classifer_f)
 #classifer_f.close()
 
-print("Naive Bayes Algo accuracy percent: ", (nltk.classify.accuracy(classifer, testing_set))*100)
+print("Original Naive Bayes Algo accuracy percent: ", (nltk.classify.accuracy(classifer, testing_set))*100)
 
 classifer.show_most_informative_features(15)
 
 #save_classifier = open("naivebayes.pickle", "wb")
 #pickle.dump(classifer, save_classifier)
 #save_classifier.close()
+
+MNB_Classifier = SklearnClassifier(MultinomialNB())
+MNB_Classifier.train(list(training_set))
+print("MNB_Classifier accuracy percent: ", (nltk.classify.accuracy(MNB_Classifier, testing_set))*100)
+
+'''
+GNB_Classifier = SklearnClassifier(GaussianNB())
+GNB_Classifier.train(list(training_set))
+print("GNB_Classifier accuracy percent: ", (nltk.classify.accuracy(GNB_Classifier, testing_set))*100)
+'''
+
+BNB_Classifier = SklearnClassifier(BernoulliNB())
+BNB_Classifier.train(list(training_set))
+print("BNB_Classifier accuracy percent: ", (nltk.classify.accuracy(BNB_Classifier, testing_set))*100)
+
+# LogisticRegression, SGDClassifier
+# SVC, LinearSVC, NuSVC
+
+LogisticRegression_Classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_Classifier.train(list(training_set))
+print("LogisticRegression_Classifier accuracy percent: ", (nltk.classify.accuracy(LogisticRegression_Classifier, testing_set))*100)
+
+SGD_Classifier = SklearnClassifier(SGDClassifier())
+SGD_Classifier.train(list(training_set))
+print("SGD_Classifier accuracy percent: ", (nltk.classify.accuracy(SGD_Classifier, testing_set))*100)
+
+SVC_Classifier = SklearnClassifier(SVC())
+SVC_Classifier.train(list(training_set))
+print("SVC_Classifier accuracy percent: ", (nltk.classify.accuracy(SVC_Classifier, testing_set))*100)
+
+LinearSVC_Classifier = SklearnClassifier(LinearSVC())
+LinearSVC_Classifier.train(list(training_set))
+print("LinearSVC_Classifier accuracy percent: ", (nltk.classify.accuracy(LinearSVC_Classifier, testing_set))*100)
+
+NuSVC_Classifier = SklearnClassifier(NuSVC())
+NuSVC_Classifier.train(list(training_set))
+print("NuSVC_Classifier accuracy percent: ", (nltk.classify.accuracy(NuSVC_Classifier, testing_set))*100)
+
 
